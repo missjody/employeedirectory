7e4d9f0800b1b8dd139b0daacc42d78a96256b09
Jody Russell <missjodyrussell@gmail.com>
2020-03-26 13:28:48 -0600

    playing with Faker going well, need to update home's rendering

--- src/components/pages/Directory.js
+++ src/components/pages/Directory.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Faker from "faker";
+
+// can generate variables here to use below
+// can generate consts in ES6 form 
+// const example = whatever => {}
+const test = "working";
 
 function Directory(props) {
+  // console.log("Directory Props: ", props);
+  console.log(Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));
   return (
     <div>
-
+      {/* {this.users.map(user => this.renderUsers(user))} */}
       {/* HTML goes here */}
-      <h2> Directory Working </h2>
+      {/* curlies is injecting info */}
+      {/* can also call functions within the curlies */}
+      <h2> Directory {test} </h2>
     </div>
   )
 }
