5047c34fabd991da024032814755afb7273e3781
Jody Russell <missjodyrussell@gmail.com>
2020-03-26 14:38:18 -0600

    moved to single page, passing props correctly, need to finish design

--- src/App.js
+++ src/App.js
@@ -1,83 +1,30 @@
 // possible option from activities 19 and 20 and review
-import React, { Component } from "react";
+import React from "react";
 import Faker from "faker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import NavTabs from "./components/NavTabs";
+// import Home from "./components/pages/Home";
 import Directory from "./components/pages/Directory";
+import EmployeeCard from "./components/EmployeCard";
 
 import './App.css'; // is this needed here? 
 
-// from fakerjs
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      users: [],
-    }
-  }
+function App() {
 
-  componentDidMount() {
-    for (let i = 0; i < 5; i++) {
-      const user = {
-        name: Faker.internet.userName(),
-        email: Faker.internet.email(),
-        avatar: Faker.internet.avatar(),
-      }
-      this.setState(prevState => ({
-        users: [...prevState.users, user],
-      }))
-    }
-  }
+  return (
+    <div>
+      <Directory />
+      <EmployeeCard
 
-  renderUsers(user) {
-    return (
-      <div style={{ border: 'solid 1px #eee' }}>
-        <img src={user.avatar} alt={user.name} width="50" height="50" />
-        <h4>Name: {user.name}</h4>
-        <h4>Email: {user.email}</h4>
-      </div>
-    )
-  }
+        name={Faker.name.findName()}
+        email={Faker.internet.email()}
+        avatar={Faker.internet.avatar()}
+        title={Faker.name.jobTitle()}
+        phone={Faker.phone.phoneNumber()}
 
-  render() {
-    return <div>
-      <Router>
-        {/* <> */}
-        {/* NavTabs will always be loaded */}
-        <NavTabs />
-
-        {/* Home has to be loaded this way 
-         otherwise it doesn't load on first open
-         of the site */}
-        <Home />
-        {/* This will be rendered conditionally */}
-        <Route exact path="/Directory" component={Directory} />
-        {/* </> */}
-      </Router>
+      />
     </div>
-  }
+  )
 }
 
-export default App
-
-//   return (
-//     // for single page application one Router
-//     <Router>
-//       <>
-//         {/* NavTabs will always be loaded */}
-//         <NavTabs />
-//         <div className="container">
-//           {/* Home has to be loaded this way 
-//         otherwise it doesn't load on first open
-//         of the site */}
-//           <Home />
-//           {/* This will be rendered conditionally */}
-//           <Route exact path="/Directory" component={Directory} />
-//         </div>
-//       </>
-//     </Router>
-//   );
-// };
-
-// export default App;
+export default App;
--- src/components/pages/Directory.js
+++ src/components/pages/Directory.js
@@ -1,21 +1,28 @@
 import React from "react";
-import Faker from "faker";
+// import Faker from "faker";
+// import EmployeeCard from "../EmployeCard.js"
 
 // can generate variables here to use below
 // can generate consts in ES6 form 
 // const example = whatever => {}
-const test = "working";
+// const test = "working";
+
+// CHANGE DIRECTORY TO JUST BE THE WRAPPER
 
 function Directory(props) {
-  // console.log("Directory Props: ", props);
-  console.log(Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));
+  console.log("Directory Props: ", props);
+  // console.log(Faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));
   return (
     <div>
+      <h1> test Directory </h1>
+
       {/* {this.users.map(user => this.renderUsers(user))} */}
       {/* HTML goes here */}
       {/* curlies is injecting info */}
       {/* can also call functions within the curlies */}
-      <h2> Directory {test} </h2>
+      {/* // <h2> Directory {test} </h2>
+      // <h5> {renderUsers()}</h5> */}
+      <h1> {props.user} </h1>
     </div>
   )
 }
