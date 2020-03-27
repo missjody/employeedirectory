1ad80beac948aaa1a024c7154c91afcbf4e79586
Jody Russell <missjodyrussell@gmail.com>
2020-03-27 16:09:49 -0600

    employees pulling in and displaying on page

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,22 +1,43 @@
-// import React from "react";
-import './App.css';
+import React from "react";
+// import './App.css';
 
 //design how each employee's card will look like
 
 const Employees = (props) => {
+
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.firstName} src={props.icon} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.firstName} {props.lastName}
+          </li>
+          <li>
+            <strong>Email:</strong> {props.email}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+
   // return what goes in the card here
   // return (
-  //   <div>
-
-  //     <img alt={props.name} src={props.avatar} />
-  //     <h1> test employee card</h1>
-  //     <p> Name: {props.name} </p>
-  //     <p> Email: {props.email}</p>
-  //     <p> phone: {props.phone} </p>
-  //     <p> title: {props.title} </p>
 
 
-  //   </div>
+  // //   <div>
+
+  // //     <img alt={props.name} src={props.avatar} />
+  // //     <h1> test employee card</h1>
+  // //     <p> Name: {props.name} </p>
+  // //     <p> Email: {props.email}</p>
+  // //     <p> phone: {props.phone} </p>
+  // //     <p> title: {props.title} </p>
+
+
+  // //   </div>
   // )
 }
 
